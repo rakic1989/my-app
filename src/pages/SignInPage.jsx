8f809d1e252a27fb51ee24a9ebae71e0a9f6c4ac
rakic1989 <rakic1989@gmail.com>
2023-05-18 15:24:43 +0200

    brisanje nekoristenog

--- src/pages/SignInPage.jsx
+++ src/pages/SignInPage.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { Navigate, Link } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import { SignInForm } from "../components/SignInForm";
 import { AppContext } from "../contexts/AppContext";
 
